f487181c1a497316b6427b4e3a0e0d2dca3dff91
Joe Hawkins <josephlhawkins2@gmail.com>
2022-04-20 11:29:09 +0100

    added article route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
+import Article from "./pages/Article/Article";
 import Articles from "./pages/Articles/Articles";
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Articles />}></Route>
+        <Route path="/" element={<Articles />}></Route>{" "}
+        <Route path="/articles/:article_id" element={<Article />}></Route>
         <Route path="/topics/:topic" element={<Articles />}></Route>
       </Routes>
     </div>
