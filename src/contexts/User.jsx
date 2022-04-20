dfaf199ab526679afb49f6a698619cab87aa7571
Joe Hawkins <josephlhawkins2@gmail.com>
2022-04-20 16:40:01 +0100

    even more styling tweaks but still ugly

--- src/contexts/User.jsx
+++ src/contexts/User.jsx
@@ -4,7 +4,7 @@ export const UserContext = createContext();
 
 export const UserProvider = (props) => {
   // There is no authentication layer so a user is hardcoded for now
-  const [user, setUser] = useState({ username: "p-copley" });
+  const [user, setUser] = useState({ username: "cooljmessy" });
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
--- src/pages/Articles/ArticleCard.jsx
+++ src/pages/Articles/ArticleCard.jsx
@@ -2,30 +2,31 @@ import "./ArticleCard.css";
 import { Link } from "react-router-dom";
 
 const ArticleCard = ({ article }) => {
-  console.log(article);
   return (
-    <Link to={`/articles/${article.article_id}`}>
-      <article className="card">
+    <article className="card">
+      <Link to={`/articles/${article.article_id}`}>
         <div className="card-body">
-          <span class="tag tag-blue">{article.topic}</span>
+          <span className={`tag tag-generic tag tag-${article.topic}`}>
+            {article.topic}
+          </span>
           <h4>{article.title}</h4>
           <p>{`${article.body.substring(0, 50)}...`}</p>
         </div>
-        <div class="card__footer">
-          <div class="user">
-            <div class="user__info">
-              <h5>{article.author}</h5>
-              <small>
-                Posted: {new Date(article.created_at).getDate()}/
-                {new Date(article.created_at).getMonth()}/
-                {new Date(article.created_at).getFullYear()}
-              </small>
-            </div>
+      </Link>
+      <div className="card__footer">
+        <div className="user">
+          <div className="user__info">
+            <h5>{article.author}</h5>
+            <small>
+              Posted: {new Date(article.created_at).getDate()}/
+              {new Date(article.created_at).getMonth()}/
+              {new Date(article.created_at).getFullYear()}
+            </small>
           </div>
         </div>
-        <p className="comment_count">Comments: {article.comment_count}</p>
-      </article>
-    </Link>
+      </div>
+      <p className="comment_count">Comments: {article.comment_count}</p>
+    </article>
   );
 };
 export default ArticleCard;
