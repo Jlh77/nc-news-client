a30821d0766c13e8ad60c14e79c7c1869dc5dc9b
Joe Hawkins <josephlhawkins2@gmail.com>
2022-05-24 14:49:45 +0100

    move auth routes to user context

--- src/contexts/User.jsx
+++ src/contexts/User.jsx
@@ -1,22 +1,82 @@
-import { createContext, useEffect, useState } from "react";
-import { getCurrentUser } from "../utils/api";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+axios.defaults.withCredentials = true;
+
+const authApi = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://nc-news77.herokuapp.com/api/auth"
+      : "http://localhost:9099/api/auth",
+});
 
 export const UserContext = createContext();
+export const useAuth = () => {
+  return useContext(UserContext);
+};
 
 export const UserProvider = (props) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [change, setChange] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
+    setIsLoading(true);
     const loadUser = async () => {
-      const getUser = await getCurrentUser();
-      console.log("New User>>>", getUser);
-      setCurrentUser(getUser);
+      await getCurrentUser();
     };
     loadUser();
-  }, []);
+    setIsLoading(false);
+  }, [change]);
+
+  const getCurrentUser = async () => {
+    const { data } = await authApi.get(`current-user`);
+    setCurrentUser(data);
+    console.log("Loaded User>>>", data);
+    return data;
+  };
+
+  const login = async (email, password) => {
+    if (currentUser) return;
+    const res = await authApi.post(`login`, { email, password });
+    setChange((change) => !change);
+    navigate("/");
+    return res;
+  };
+
+  const register = async (email, username, password) => {
+    if (currentUser) return;
+    const res = await authApi.post(`join`, { email, username, password });
+    setChange((change) => !change);
+    navigate("/t");
+    return res;
+  };
+
+  const logout = async () => {
+    const res = await authApi.post(`logout`);
+    setCurrentUser(null);
+    navigate("login");
+    return res;
+  };
+
+  // export const getCSRFToken = async () => {
+  //   const res = await newsApi.get(`getCSRFToken`);
+  //   newsApi.defaults.headers.post["X-CSRF-Token"] = res.data.CSRFToken;
+  //   return res.data;
+  // };
 
   return (
-    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+    <UserContext.Provider
+      value={{
+        currentUser,
+        setCurrentUser,
+        login,
+        register,
+        logout,
+        getCurrentUser,
+      }}
+    >
       {props.children}
     </UserContext.Provider>
   );
